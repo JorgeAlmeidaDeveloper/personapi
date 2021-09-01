3e7af2f469fb382569f4d4d22609dd72072298e8
JorgeAlmeidaDeveloper <jorge.rummenigg@hotmail.com>
2021-09-01 14:56:13 +0100

    vtes

--- src/main/java/one/dio/personapi/controller/PersonController.java
+++ src/main/java/one/dio/personapi/controller/PersonController.java
@@ -1,16 +1,33 @@
 package one.dio.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.dio.personapi.dto.MessageResponseDto;
+import one.dio.personapi.entity.Person;
+import one.dio.personapi.repositery.PersonRepositery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
+
+    private PersonRepositery personRepositery;
+
+    @Autowired
+    public PersonController(PersonRepositery personRepositery) {
+        this.personRepositery = personRepositery;
+    }
 
     @GetMapping
     public String getBook(){
         return "Api Teste!!!!";
     }
 
+    @PostMapping
+    public MessageResponseDto createPerson(@RequestBody Person person){
+        personRepositery.save(person);
+        return MessageResponseDto
+                .builder()
+                .message("Criou seu person ID")
+                .build();
+    }
 }
--- src/main/java/one/dio/personapi/repositery/PersonRepositery.java
+++ src/main/java/one/dio/personapi/repositery/PersonRepositery.java
@@ -2,6 +2,7 @@ package one.dio.personapi.repositery;
 
 import lombok.*;
 import one.dio.personapi.entity.Person;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
